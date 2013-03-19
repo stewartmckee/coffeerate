397a6fb4c09fc9c98f98cd9afcf96296501f2faf
Stewart McKee <stewart@theizone.co.uk>
2013-03-19 14:36:20 +0000

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeerate::Application.config.secret_token = '44805bbb4d56270449a74eb0d3a1521e8e18f8e4fdaa78f34f8edd0065ba4c5df6cf4e91d791c22563885fbe701583bd83eb6d12e2cf6ffea5cc203ea169fedd'
