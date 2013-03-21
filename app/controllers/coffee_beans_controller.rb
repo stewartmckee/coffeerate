4cf06e6574142519a24e03c93a7a1db5bce6f0e7
Stewart McKee <stewart@theizone.co.uk>
2013-03-21 13:46:29 +0000

    added authenticate_user

--- app/controllers/coffee_beans_controller.rb
+++ app/controllers/coffee_beans_controller.rb
@@ -1,6 +1,8 @@
 class CoffeeBeansController < ApplicationController
   # GET /coffee_beans
   # GET /coffee_beans.json
+  before_filter :authenticate_user!
+  
   def index
     @coffee_beans = CoffeeBean.all
 
